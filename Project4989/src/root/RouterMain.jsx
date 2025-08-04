65684bbd943ecad64cc5d75f3b5bcd166e264fd1
hjyoo <hjyoo330@gmail.com>
2025-08-04 22:13:38 +0900

    post

--- Project4989/src/root/RouterMain.jsx
+++ Project4989/src/root/RouterMain.jsx
@@ -2,31 +2,30 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from '../layout/MainLayout'
 import MainPage from '../main/MainPage'
-import Chat from '../chat/chat'
 import Goods from '../board/Goods'
 import Cars from '../board/Cars'
 import Real_estate from '../board/Real_estate'
+import Auction from '../board/Auction'
 import LoginForm from '../login/LoginForm'
 import SignupForm from '../login/SignupForm'
-import AuctionMain from '../auction/auction_main/AuctionMain'
-import Post from '../board/Post'
+import KakaoMap from '../chat/KakaoMap'
 
 const RouterMain = () => {
   return (
     <div>
       <Routes>
-        <Route element={<MainLayout/>} >
-            <Route path='/' element={<MainPage/>}/>
-            <Route path='/chat' element={<Chat/>}/>
-            <Route path='/cars' element={<Cars/>}/>
-            <Route path='/goods' element={<Goods/>}/>
-            <Route path='/real-estate' element={<Real_estate/>}/>
-            <Route path='/auction' element={<AuctionMain/>}/>
-            <Route path='/login' element={<LoginForm/>}/>
-            <Route path='/signup' element={<SignupForm/>}/>
+        <Route element={<MainLayout />} >
+          <Route path='/' element={<MainPage />} />
+          <Route path='/cars' element={<Cars />} />
+          <Route path='/goods' element={<Goods />} />
+          <Route path='/real-estate' element={<Real_estate />} />
+          <Route path='/auction' element={<Auction />} />
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/signup' element={<SignupForm />} />
+          <Route path='/map' element={<KakaoMap />} />
         </Route>
 
-        
+
       </Routes>
     </div>
   )
