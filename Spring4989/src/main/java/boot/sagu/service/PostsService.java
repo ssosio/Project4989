a88db3522e697c1a684e95163331d85f865b50e2
hjyoo <hjyoo330@gmail.com>
2025-08-04 15:46:48 +0900

    proc

--- Spring4989/src/main/java/boot/sagu/service/PostsService.java
+++ Spring4989/src/main/java/boot/sagu/service/PostsService.java
@@ -1,8 +1,22 @@
 package boot.sagu.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class PostsService {
+import boot.sagu.dto.PostsDto;
+import boot.sagu.mapper.PostsMapper;
 
+@Service
+public class PostsService implements PostsServiceInter{
+	
+	@Autowired
+	private PostsMapper postMapper;
+
+	@Override
+	public void insertPost(PostsDto pdto) {
+		// TODO Auto-generated method stub
+		postMapper.insertPost(pdto);
+		
+	}
+	
 }
---
+++ Spring4989/src/main/java/boot/sagu/service/PostsServiceInter.java
@@ -0,0 +1,7 @@
+package boot.sagu.service;
+
+import boot.sagu.dto.PostsDto;
+
+public interface PostsServiceInter {
+	public void insertPost(PostsDto pdto);
+}
