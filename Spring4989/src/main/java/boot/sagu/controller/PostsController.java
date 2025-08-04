ad5894a5d7880c1b1caaf869f2ddc4c2b08bef56
hjyoo <hjyoo330@gmail.com>
2025-08-04 17:54:43 +0900

    post

--- Spring4989/src/main/java/boot/sagu/controller/PostsController.java
+++ Spring4989/src/main/java/boot/sagu/controller/PostsController.java
@@ -1,20 +1,33 @@
 package boot.sagu.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import boot.sagu.dto.PostsDto;
 import boot.sagu.service.PostsService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5176/")
+@CrossOrigin(origins = "*")
+@RequestMapping("/post")
 public class PostsController {
 	
 	@Autowired
 	private PostsService postService;
+	
+	@GetMapping("/list")
+	public List<PostsDto> list()
+	{
+		return postService.getAllPostData();
+	}
+	
+	
 	
 	@PostMapping("/upload")
 	public String fileUpload() {
--- Spring4989/src/main/java/boot/sagu/service/PostsServiceInter.java
+++ Spring4989/src/main/java/boot/sagu/service/PostsServiceInter.java
@@ -1,7 +1,11 @@
 package boot.sagu.service;
 
+import java.util.List;
+
 import boot.sagu.dto.PostsDto;
 
 public interface PostsServiceInter {
 	public void insertPost(PostsDto pdto);
+	public List<PostsDto> getAllPostData();
+	public PostsDto getPostData(int post_id);
 }
