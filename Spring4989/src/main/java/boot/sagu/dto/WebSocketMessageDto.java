a0e2db5f3355c9bc007580b98a124e05c1d6bcab
parkib002 <parkib003@naver.com>
2025-08-05 14:45:52 +0900

    4989

---
+++ Spring4989/src/main/java/boot/sagu/dto/WebSocketMessageDto.java
@@ -0,0 +1,15 @@
+package boot.sagu.dto;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class WebSocketMessageDto {
+    private String type; // "CHAT", "JOIN", "LEAVE" 등
+    private Long chatRoomId;
+    private Long senderId;
+    private String messageContent;
+    private String messageType;
+    private Timestamp timestamp;
+} 
