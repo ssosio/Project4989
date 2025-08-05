544bc033b35043c02dc097ed07e4ed741e37fb97
hjyoo <hjyoo330@gmail.com>
2025-08-05 15:16:26 +0900

    post

--- Spring4989/src/main/java/boot/sagu/controller/AuctionController.java
+++ Spring4989/src/main/java/boot/sagu/controller/AuctionController.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import boot.sagu.dto.PostsDto;
 import boot.sagu.mapper.AuctionMapper;
-import boot.sagu.mapper.PostsMapper;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5176/")
--- Spring4989/src/main/java/boot/sagu/controller/PostsController.java
+++ Spring4989/src/main/java/boot/sagu/controller/PostsController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,6 @@ public class PostsController {
 	@Autowired
 	private PostsService postService;
 	
-	String photoName;
 	
 	@GetMapping("/list")
 	public List<PostsDto> list()
@@ -41,33 +41,15 @@ public class PostsController {
 	@PostMapping("/upload")
 	public String fileUpload(@RequestParam("uploadFile") MultipartFile uploadFile,HttpSession session) 
 	{
-		String fileName=uploadFile.getOriginalFilename();
 		
-		String path=session.getServletContext().getRealPath("/save");
-		
-		File file=new File(path+"\\"+photoName);
-		
-		SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMddHHmmss");
-		photoName=sdf.format(new Date())+uploadFile.getOriginalFilename();
-		System.out.println(photoName);
-		
-		try {
-			uploadFile.transferTo(new File(path+"\\"+photoName));
-		} catch (IllegalStateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return photoName;
+		return null;
 	}
 	
 	@PostMapping("/insert")
-	public void insertPost(@RequestBody PostsDto pdto)
+	public void insertPostWithPhoto(@ModelAttribute PostsDto pdto,@RequestParam("uploadFiles") List<MultipartFile> uploadFiles,
+		    HttpSession session)
 	{
-		
+		postService.insertPostWithPhoto(pdto, uploadFiles, session);
 	}
 
 }
