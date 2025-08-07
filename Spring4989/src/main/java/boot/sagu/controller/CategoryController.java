57a26bf05de913c2bef45e84ef6652c73618d919
hjyoo <hjyoo330@gmail.com>
2025-08-07 09:39:49 +0900

    post

--- Spring4989/src/main/java/boot/sagu/controller/CategoryController.java
+++ Spring4989/src/main/java/boot/sagu/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import boot.sagu.dto.CategoryDto;
 import boot.sagu.service.CategoryService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/category")
 public class CategoryController {
 	
